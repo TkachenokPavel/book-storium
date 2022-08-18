13f2aa2b8ad115afaf6a770a2ce11081eceb1521
TkachenokPavel <pasha.tkachenok@mail.ru>
2022-08-18 10:40:03 +0300

    feat(routing): create index file in pages folder and add paths to routes

---
+++ src/pages/index.ts
@@ -0,0 +1,19 @@
+import { Account } from "./Account";
+import { Book } from "./Book";
+import { Cart } from "./Cart";
+import { Favorites } from "./Favorites";
+import { Home } from "./Home";
+import { Reset } from "./Reset";
+import { Search } from "./Search";
+import { SignIn } from "./SignIn";
+
+export {
+    Account,
+    Book,
+    Cart,
+    Favorites,
+    Home,
+    Reset,
+    Search,
+    SignIn
+}
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,7 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+import { Home, Book, Cart, Account, Favorites, Reset, SignIn, Search } from "../pages";
 
 export const AppRouter = () => {
     return <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/book" element={<Book />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/reset" element={<Reset />} />
+        <Route path="/sign-in" element={<SignIn />} />
     </Routes>
 }
