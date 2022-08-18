0ad0e72ef54ce63e1dc3992d9aa8b8ee9bc7ad38
TkachenokPavel <pasha.tkachenok@mail.ru>
2022-08-18 19:05:56 +0300

    feat(routing): complete routing task

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,11 @@
 export enum routes {
     HOME = "/",
-    SEARCH = "/search",
-    BOOK = "/book",
-    CART = "/cart",
-    ACCOUNT = "/account",
-    FAVORITES = "/favorites",
-    RESET = "/reset",
-    SIGN_IN = "/sign-in",
+    SEARCH = "search",
+    BOOK = "book",
+    CART = "cart",
+    ACCOUNT = "account",
+    FAVORITES = "favorites",
+    RESET = "reset",
+    SIGN_IN = "sign-in",
     NOT_FOUND = "*",
 }
