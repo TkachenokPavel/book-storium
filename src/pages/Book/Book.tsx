108c3aa43d219b31cc0fc9bef2452f9eeb5300f2
TkachenokPavel <pasha.tkachenok@mail.ru>
2022-08-24 18:47:53 +0300

    feat(book-api): create typing to book details

--- src/pages/Book/Book.tsx
+++ src/pages/Book/Book.tsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom'
 import { ArrowBack } from '../../assets'
 import { bookAPI } from '../../services/bookApi/bookApi';
+import { IBookDetails } from '../../types/types';
 
 export const Book = () => {
     const navigate = useNavigate();
     const { isbn } = useParams();
-    const [book, setBook] = useState({});
+    const [book, setBook] = useState<IBookDetails | null>(null);
 
     const handleArrow = () => {
         navigate(-1);
