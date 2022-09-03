9dde78c4f7cadaca04e43685887fa40cc9bb4792
TkachenokPavel <pasha.tkachenok@mail.ru>
2022-09-03 19:18:37 +0300

    feat(authentication): add styles to authentication page

--- src/pages/Authentication/Authentication.tsx
+++ src/pages/Authentication/Authentication.tsx
@@ -1,9 +1,51 @@
-import { StyledAuthentication } from "./styles"
+import { useState } from "react"
+import { SignIn } from "../../components/SignIn"
+import { SignUp } from "../../components/SignUp"
+import { SignInSwitch, SignUpSwitch, StyledAuthentication, SwitchWrapper, Wrapper } from "./styles"
+
+export interface IFormSwitch {
+    signIn: boolean,
+    signUp: boolean
+}
 
 export const Authentication = () => {
+    const [formSwitch, setFormSwitch] = useState<IFormSwitch>({
+        signIn: true,
+        signUp: false
+    })
+
+    const handleSignInSwitch = () => {
+        setFormSwitch((actual) => ({
+            signIn: true,
+            signUp: false
+        }))
+    }
+
+    const handleSignUpSwitch = () => {
+        setFormSwitch((actual) => ({
+            signIn: false,
+            signUp: true
+        }))
+    }
+
     return (
         <StyledAuthentication>
-            wd
+            <Wrapper>
+                <SwitchWrapper>
+                    <SignInSwitch
+                        $formSwitch={formSwitch.signIn}
+                        onClick={handleSignInSwitch}>
+                        sign in
+                    </SignInSwitch>
+                    <SignUpSwitch
+                        $formSwitch={formSwitch.signUp}
+                        onClick={handleSignUpSwitch}>
+                        sign up
+                    </SignUpSwitch>
+                </SwitchWrapper>
+                {formSwitch.signIn && <SignIn />}
+                {formSwitch.signUp && <SignUp />}
+            </Wrapper>
         </StyledAuthentication>
     )
 }
--- src/pages/Authentication/styles.ts
+++ src/pages/Authentication/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { Color, H2, H3 } from '../../ui';
+import { IFormSwitch } from './Authentication';
 
 
 export const StyledAuthentication = styled.div`
@@ -6,4 +8,55 @@ export const StyledAuthentication = styled.div`
   place-items: center;
 
   min-height: 70vh;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 544px;
+  padding: 0px 32px 40px;
+
+  border: 1px solid ${Color.SECONDARY};
+`;
+
+export const SwitchWrapper = styled.div`
+  display: flex;
+
+  width: 100%;
+  height: 72px;
+`;
+
+export const SignInSwitch = styled.div<{ $formSwitch: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  width: 50%;
+  
+  ${H3};
+  text-transform: uppercase ;
+
+  border-bottom: 1px solid;
+  border-color: ${(({ $formSwitch }) => $formSwitch ? Color.PRIMARY : Color.SECONDARY)};
+  color: ${(({ $formSwitch }) => $formSwitch ? Color.PRIMARY : Color.SECONDARY)};
+
+  cursor: pointer;
+`;
+
+export const SignUpSwitch = styled.div<{ $formSwitch: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 50%;
+
+  ${H3};
+  text-transform: uppercase ;
+
+  border-bottom: 1px solid ;
+  border-color: ${(({ $formSwitch }) => $formSwitch ? Color.PRIMARY : Color.SECONDARY)};
+  color: ${(({ $formSwitch }) => $formSwitch ? Color.PRIMARY : Color.SECONDARY)};
+
+  cursor: pointer;
 `;
